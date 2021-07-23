6c9fa4a2df5340d93c6bd0bee22304060b2be210
Valentin <vvvaleo@gmail.com>
2021-07-23 13:33:45 +0300

    ishodnik

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("думающие нажраться лучше как,-");
+        System.out.println("имеющим ванную и теплый клозет!");
+        System.out.println("Я лучше в баре … буду");
+        System.out.println("вдруг увидел, израненный,");
+        System.out.println();
+        System.out.println("подавать ананасную воду!");
+        System.out.println("Вам, проживающим за оргией оргию,");
+        System.out.println("может быть, сейчас бомбой ноги");
+        System.out.println("выдрало у Петрова поручика?..");
+        System.out.println();
+        System.out.println("Вам ли, любящим баб да блюда,");
+        System.out.println("Как вам не стыдно о представленных к Георгию");
+        System.out.println("жизнь отдавать в угоду?!");
+        System.out.println("похотливо напеваете Северянина!");
+        System.out.println();
+        System.out.println("Знаете ли вы, бездарные, многие,");
+        System.out.println("как вы измазанной в котлете губой");
+        System.out.println("Если он приведенный на убой,");
+        System.out.println("вычитывать из столбцов газет?");
+    }
+}
